77c98eb443c665fe958849535da37b977b6cf5ad
Jose Evandro <evandrocartman2015@gmail.com>
2023-04-28 19:41:13 -0300

    ajuste no layout do mainheader

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,14 +1,18 @@
 <template>
     <v-app-bar height="80px" outlined extension-height="64" color="primary" app asolute>
-        <v-btn icon color="white" link to="/">
-            <v-icon>mdi-web</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <div>
-            <v-btn icon color="white" link to="/perfil">
-                <v-icon>mdi-account</v-icon>
-            </v-btn>
-        </div>
+        <v-row style="display: flex; align-items: center; justify-content: space-between; padding: 0 2rem;">
+            <div>
+                <v-btn icon color="white" link to="/">
+                    <v-icon>mdi-web</v-icon>
+                </v-btn>
+            </div>
+            <div>
+                <v-btn icon color="white" link to="/perfil">
+                    <v-icon>mdi-account</v-icon>
+                </v-btn>
+            </div>
+        </v-row>
+
     </v-app-bar>
 </template>
 
