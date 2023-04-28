a00d32b170884a8a9c4f9c8d528ea09d3e4cc399
Jose Evandro <evandrocartman2015@gmail.com>
2023-04-28 17:58:26 -0300

    ajustes layoute e paletta de cores na pagina de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,30 @@
 <template>
-      <v-app>
+      <v-app style="background-color: #D8B2F9;">
             <v-container class="fill-height">
                   <v-row allign="center" justify="center">
-                        <v-col cols="12" sm="10">
+                        <v-col md="12" sm="10">
                               <v-card class="elevation-6 mt-10" color="primary" width="auto">
                                     <v-window v-model="step">
                                           <v-window-item :value="1">
                                                 <v-row>
                                                       <v-col cols="12" sm="6">
                                                             <v-card-title
-                                                                  class="text-h6 font-weight-regular justify-space-between">
+                                                                  class="text-h5 font-weight-regular justify-space-between">
                                                                   <span>{{ currentTitle }}</span>
-                                                                  <v-avatar color="primary lighten-2"
-                                                                        class="subheading white--text" size="24"
-                                                                        v-text="step"></v-avatar>
                                                             </v-card-title>
                                                             <v-card-text>
-                                                                  <v-text-field class="mt-15" append-icon="mdi-account"
-                                                                        outlined label="Usuario/E-mail"></v-text-field>
-                                                                  <v-text-field append-icon="mdi-lock" label="Senha"
-                                                                        type="password" outlined></v-text-field>
-                                                                  <v-btn rounded block dark color="confirm">Login</v-btn>
+                                                                  <v-row allign="center" justify="center" class="mt-15">
+                                                                        <v-col cols="12" sm="8">
+                                                                              <v-text-field append-icon="mdi-account" outlined
+                                                                                    label="Usuario/E-mail"></v-text-field>
+                                                                              <v-text-field append-icon="mdi-lock"
+                                                                                    label="Senha" type="password"
+                                                                                    outlined></v-text-field>
+                                                                              <v-btn rounded block dark
+                                                                                    color="confirm">Login</v-btn>
+                                                                        </v-col>
+                                                                  </v-row>
+
                                                             </v-card-text>
                                                       </v-col>
                                                       <v-col cols="12" sm="6" class="purple rounded-bl-xl">
@@ -62,21 +66,40 @@
                                                       </v-col>
                                                       <v-col cols="12" sm="6">
                                                             <v-card-title
-                                                                  class="text-h6 font-weight-regular justify-space-between">
+                                                                  class="text-h5 font-weight-regular justify-space-between">
                                                                   <span>{{ currentTitle }}</span>
-                                                                  <v-avatar color="primary lighten-2"
-                                                                        class="subheading white--text" size="24"
-                                                                        v-text="step"></v-avatar>
                                                             </v-card-title>
                                                             <v-card-text>
-                                                                  <v-text-field class="mt-15" append-icon="mdi-account"
-                                                                        outlined label="Usuario/E-mail"></v-text-field>
-                                                                  <v-text-field append-icon="mdi-lock" label="Senha"
-                                                                        type="password" outlined></v-text-field>
-                                                                  <v-text-field append-icon="mdi-lock"
-                                                                        label="Confirme a senha" type="password"
-                                                                        outlined></v-text-field>
-                                                                  <v-btn rounded block dark color="confirm">Login</v-btn>
+                                                                  <v-row allign="center" justify="center">
+                                                                        <v-col cols="12" sm="8" class="mt-10">
+                                                                              <v-row>
+                                                                                    <v-col cols="12" sm="6">
+                                                                                          <v-text-field
+                                                                                                append-icon="mdi-account"
+                                                                                                outlined
+                                                                                                label="Nome"></v-text-field>
+                                                                                    </v-col>
+                                                                                    <v-col cols="12" sm="6">
+                                                                                          <v-text-field
+                                                                                                append-icon="mdi-account"
+                                                                                                outlined
+                                                                                                label="Sobrenome"></v-text-field>
+                                                                                    </v-col>
+                                                                              </v-row>
+                                                                              <v-text-field append-icon="mdi-account" outlined
+                                                                                    label="Usuario/E-mail"></v-text-field>
+                                                                              <v-text-field append-icon="mdi-lock"
+                                                                                    label="Senha" type="password"
+                                                                                    outlined></v-text-field>
+                                                                              <v-text-field append-icon="mdi-lock"
+                                                                                    label="Confirme a senha" type="password"
+                                                                                    outlined></v-text-field>
+                                                                              <v-btn rounded block dark
+                                                                                    color="confirm">Login</v-btn>
+                                                                        </v-col>
+
+                                                                  </v-row>
+
                                                             </v-card-text>
                                                       </v-col>
                                                 </v-row>
